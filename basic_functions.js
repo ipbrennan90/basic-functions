64537ab106a1090ca67cdcd8af40d928d8377138
ipbrennan <ian.brennan@icloud.com>
2017-01-15 08:32:50 -0700

    added better description for oven function

--- basic_functions.js
+++ basic_functions.js
@@ -4,8 +4,9 @@ var speak
 // make this a function that returns a value given to it
 var returnMe
 
-// make this a function that will turn the oven on and off,
-// it should take a string as an argument and return a string
+// make this a function that will turn the oven on and off.
+// It should take a string ("on" or "off") as an argument,
+// and return the string.
 var oven
 
 // create a function that takes any number of cups, doubles
